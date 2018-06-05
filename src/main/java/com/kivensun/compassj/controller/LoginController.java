d126e48ac30596f48b9e4b2b21a4ae3afc54f131
kivensun <kivensun@gmail.com>
2018-06-05 21:31:34 +0800

    20180605 a litter change

--- src/main/java/com/kivensun/compassj/controller/LoginController.java
+++ src/main/java/com/kivensun/compassj/controller/LoginController.java
@@ -2,10 +2,12 @@ package com.kivensun.compassj.controller;
 
 import com.kivensun.compassj.domain.User;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class LoginController {
 
     @PostMapping("/login")
