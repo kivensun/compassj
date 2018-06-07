abc7d5a57facddc496c3a7d72e56ac5ed4932fb3
kivensun <kivensun@gmail.com>
2018-06-07 16:06:43 +0800

    first token

--- src/main/java/com/kivensun/compassj/CompassjApplication.java
+++ src/main/java/com/kivensun/compassj/CompassjApplication.java
@@ -1,10 +1,63 @@
 package com.kivensun.compassj;
 
+import com.kivensun.compassj.system.JwtAuthenticationFilter;
+import com.kivensun.compassj.util.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+import static com.kivensun.compassj.util.JwtUtil.USER_NAME;
 
 @SpringBootApplication
+@RestController
 public class CompassjApplication {
+
+	@GetMapping("/api/protected")
+	public @ResponseBody Object hellWorld(@RequestParam(value = USER_NAME) String userId) {
+		return "Your user id is '" + userId + "'" ;
+	}
+
+	@PostMapping("/login")
+	public Object login(HttpServletResponse response, @RequestBody final Account account) throws IOException {
+		System.out.println(account.username);
+		if(isValidPassword(account)) {
+			String jwt = JwtUtil.generateToken(account.username);
+			return new HashMap<String,String>(){{
+				put("token", jwt);
+			}};
+		}else {
+			return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+		}
+	}
+
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		JwtAuthenticationFilter filter = new JwtAuthenticationFilter();
+		registrationBean.setFilter(filter);
+		return registrationBean;
+	}
+
+	private boolean isValidPassword(Account credentials) {
+		return "admin".equals(credentials.username)
+				&& "admin".equals(credentials.password);
+	}
+
+
+	public static class Account {
+		public String username;
+		public String password;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompassjApplication.class, args);
--- src/main/java/com/kivensun/compassj/controller/LoginController.java
+++ src/main/java/com/kivensun/compassj/controller/LoginController.java
@@ -6,11 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/user")
+
 public class LoginController {
 
-    @PostMapping("/login")
+
     public boolean loginUser(@RequestParam("username") String userName,
                              @RequestParam("pwd") String userPwd){
         User user = new User();
